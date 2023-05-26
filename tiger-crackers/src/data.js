1ec2e27cfc7ea8cadff4df9125df18969f9d1d06
Santosh-24 <jaggasantosh22@gmail.com>
2023-05-26 11:19:28 +0530

    Added-products

---
+++ tiger-crackers/src/data.js
@@ -0,0 +1,38 @@
+const data = {
+  products: [
+    {
+      name: "Chakkras",
+      slug: "Diwali-Chakkaras",
+      category: "fancy",
+      image: "/images/Chakkras.jpg",
+      items: 8,
+      price: 80,
+    },
+    {
+      name: "Flower pots",
+      slug: "Diwali-FlowerPots",
+      category: "fancy",
+      image: "/images/Flower-pots.jpg",
+      items: 8,
+      price: 80,
+    },
+    {
+      name: "Atom Bombs",
+      slug: "Diwali-AtomBombs",
+      category: "Bombs",
+      image: "/images/atom-bombs.png",
+      items: 8,
+      price: 60,
+    },
+    {
+      name: "Chakkras",
+      slug: "Diwali-Chakkarass",
+      category: "fancy",
+      image: "/images/Chakkras.jpg",
+      items: 8,
+      price: 80,
+    },
+  ],
+};
+
+export default data;
