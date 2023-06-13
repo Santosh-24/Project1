71ed8abb9418db8be3dbb0be76a781fd3fd41127
Santosh-24 <jaggasantosh22@gmail.com>
2023-06-13 16:51:14 +0530

    create order screen

--- tiger-crackers/src/screens/ProductScreen.js
+++ tiger-crackers/src/screens/ProductScreen.js
@@ -8,6 +8,9 @@ import Card from "react-bootstrap/Card";
 import Badge from "react-bootstrap/Badge";
 import Button from "react-bootstrap/Button";
 import { Helmet } from "react-helmet-async";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import { getError } from "../utils";
 import { Store } from "../Store";
 
 const reducer = (state, action) => {
@@ -40,7 +43,7 @@ const ProductScreen = () => {
         const result = await axios.get(`/api/products/slug/${slug}`);
         dispatch({ type: "FETCH_SUCCESS", payload: result.data });
       } catch (err) {
-        dispatch({ type: "FETCH_FAIL", payload: err.message });
+        dispatch({ type: "FETCH_FAIL", payload: getError(err) });
       }
     };
     fetchData();
@@ -62,9 +65,9 @@ const ProductScreen = () => {
     navigate("/cart");
   };
   return loading ? (
-    <div>Loading...</div>
+    <LoadingBox />
   ) : error ? (
-    <div>{error}</div>
+    <MessageBox variant="danger">{error}</MessageBox>
   ) : (
     <Row>
       <Col md={6}>
